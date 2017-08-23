bf6bf5927704917896416d86e646e51b704b2852
Максим Цвірінькало <makstsvirinkalo@gmail.com>
2017-08-23 15:31:04 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,28 +1,24 @@
-function print(value){
-    document.write(value);
-    document.write("<br>");
-}
-const a = parseFloat(prompt("Enter a") );
-const b = parseFloat(prompt("Enter b") );
-const c = parseFloat(prompt("Enter c") );
+const a = parseFloat(prompt('Enter A digit'));
+const b = parseFloat(prompt('Enter B digit'));
+const c = parseFloat(prompt('Enter C digit'));
 
-const solution = solveQuader(a, b, c);
-print(solution);
+const solution = quadraticEquation(a, b, c);
+document.write(solution);
 
-function solveQuader(a, b, c){
-   const d = discr();
-   function discr(a, b, c){
+function discriminant (a, b, c){
     return Math.pow(b, 2) - 4 * (a * c);
-    }
-    let x1, x2;
-   
-    if(d === 0){
-        return x1, x2 = -b / (2 * a);
-    }else if(d > 0){
-        return x1 = ( Math.pow(b, 2) + Math.sqrt(d) ) / (2 * a); 
-        return x2 = ( Math.pow(b, 2) - Math.sqrt(d) ) / (2 * a);
-    }else{
-        return x1, x2 = "No answer";
-    }
-    return "x1= " + x1 + ", x2 = " + x2;
 }
+
+function quadraticEquation (a, b, c){
+    if (a === 0) {
+        return  -c / b;
+    }  
+    const d = discriminant(a, b, c);
+    if (d > 0) {
+        return [ (-b + Math.sqrt(d)) / (2 * a),
+                 (-b - Math.sqrt(d)) / (2 * a) ];
+    } else if (d === 0) {
+        return (-b - Math.sqrt(d)) / (2 * a);
+    }
+        return "There is no equation of real roots.";
+} 
